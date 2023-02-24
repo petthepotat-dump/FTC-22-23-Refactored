0a3ac019bb0a1ac00d70bd26c0dac5bd034fc26f
Darren Chu <darren.cw.chu@gmail.com>
2023-02-23 19:00:21 -0500

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/VisionTest.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.wrappers.DetectConeDisplay;
 import org.firstinspires.ftc.teamcode.wrappers.DisplayVision;
+import org.firstinspires.ftc.teamcode.wrappers.MecanumChassis;
 import org.firstinspires.ftc.teamcode.wrappers.Vision;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -13,18 +16,22 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name = "VisionTest")
 public class VisionTest extends LinearOpMode {
-    OpenCvCamera camera;
+    private OpenCvCamera camera;
+    private FtcDashboard dashboard = FtcDashboard.getInstance();
+    private DetectConeDisplay detection = new DetectConeDisplay(true);
+    private MecanumChassis robot;
     @Override
     public void runOpMode() throws InterruptedException {
-        DisplayVision sleeveDetection = new DisplayVision();
+        robot = new MecanumChassis(hardwareMap);
+        telemetry = dashboard.getTelemetry();
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Camera"), cameraMonitorViewId);
-        camera.setPipeline(sleeveDetection);
+        camera.setPipeline(detection);
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
-                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-                FtcDashboard.getInstance().startCameraStream(camera, 30);
+                camera.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+                dashboard.startCameraStream(camera, 30);
             }
 
             @Override
@@ -32,13 +39,37 @@ public class VisionTest extends LinearOpMode {
             }
         });
         while (!isStarted()) {
-            telemetry.addData("route",sleeveDetection.route);
-            // telemetry add sleeveDetection.color
-            
+            telemetry.addData("hue", detection.hsvColor[0]);
+            telemetry.addData("Saturation", detection.hsvColor[1]);
+            telemetry.addData("Value", detection.hsvColor[2]);
+            telemetry.addData("x", detection.x);
+            telemetry.addData("y", detection.y);
+            telemetry.addData("height", detection.height);
+            telemetry.addData("width", detection.width);
+
             telemetry.update();
+            sleep(200);
         }
+        goToCone();
         camera.closeCameraDeviceAsync(() -> {
-            FtcDashboard.getInstance().stopCameraStream();
+            dashboard.stopCameraStream();
         });
+    }
+    private void move(double fr, double fl, double br, double bl) {
+        robot.fr.setPower(fr);
+        robot.fl.setPower(fl);
+        robot.br.setPower(br);
+        robot.bl.setPower(bl);
+    }
+    private void goToCone() {
+        while (detection.x<118 || detection.x>128 || detection.width<150) {
+            double error = 123-detection.x, distanceError = detection.width-150;
+            double power = error/250, distancePower = distanceError/250;
+            move(-power+distancePower, power+distancePower, power+distancePower, -power+distancePower);
+            telemetry.addData("error", error);
+            telemetry.addData("distance_error", distanceError);
+            telemetry.update();
+            sleep(100);
+        }
     }
 }
